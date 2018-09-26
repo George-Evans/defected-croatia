00144cf9b511286aaea62e21fa90f319670d1813
George Evans <george.r.evans@googlemail.com>
2018-09-26 17:13:47 +0200

    new colours, navbar background and programme page content

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ $("#year").append(year);
 $('#date-countdown').countdown('2019/08/08 14:00:00', function(event){
 	$(this).html(event.strftime('<span class="countdown">%D</span> DAYS &nbsp;<span class="countdown">%H</span> HR &nbsp;<span class="countdown">%M</span> MIN &nbsp;<span class="countdown">%S</span> SEC'));
 });
+
+// Navbar BG on scroll
+var navbarHeight = $('.navbar').outerHeight();
+$(window).scroll(function () {
+	if ($(window).scrollTop() > navbarHeight) {
+		$('.navbar-bg').height(navbarHeight + 20);
+	} else {
+		$('.navbar-bg').height('20px');
+	}
+});
